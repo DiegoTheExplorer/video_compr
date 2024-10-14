354eeb50c36b7ccf96ab8968bfa16d54e1c75f2c
Diego Miguel M. Villamil <dmvillamil@up.edu.ph>
2024-10-14 17:41:41 +0800

    currently converts to h265, yuv420p pixel format, and to 24 fps to compress videos

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
+/*
+ffmpeg video compression reference: https://shotstack.io/learn/compress-video-ffmpeg/
+*/
 func main() {
 	inp_dir := "../videos/raw/"
 	files, err := os.ReadDir(inp_dir)
@@ -20,7 +23,8 @@ func main() {
 
 	for ind, file := range files {
 		inp_path = inp_dir + file.Name()
-		err = ffmpeg.Input(inp_path).Output(out_path+file.Name(), ffmpeg.KwArgs{"an": "", "c": "copy"}).
+		err = ffmpeg.Input(inp_path).
+			Output(out_path+file.Name(), ffmpeg.KwArgs{"an": "", "c:v": "libx265", "pix_fmt": "yuv420p", "r": 24}).
 			OverWriteOutput().
 			Run()
 		if err != nil {
